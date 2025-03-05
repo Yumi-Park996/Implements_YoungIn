d3b856fe08d95959d3dcbdfcee56cb09af677432
qus0in <qus0in@gmail.com>
2025-03-05 11:42:22 +0900

    csv

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,17 @@
+import util.file.CSVReader;
+import util.file.CSVReaderImpl;
+
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
+
 public class Application {
     public static void main(String[] args) {
-
+        Logger logger = Logger.getLogger(Application.class.getName());
+        CSVReader reader = new CSVReaderImpl();
+        try {
+            reader.read("til.csv");
+        } catch (FileNotFoundException e) {
+            logger.severe(e.getMessage());
+        }
     }
 }
